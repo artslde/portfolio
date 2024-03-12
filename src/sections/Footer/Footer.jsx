c9f424710af5181775d95f70238dc89f662cc50b
artslde <artside58@gmail.com>
2024-03-12 21:43:44 +0300

    other fix

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,9 +1,13 @@
-import {Image} from "../component/Image/Image.jsx"
+import { Image } from "../component/Image/Image.jsx"
 import styles from "./Footer.module.css"
 
 export const Footer = () => (
 	<footer className={styles.footer}>
-		<span className={styles.footerText}>Не копируйте пожалуйста {"(⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)"}</span>
+		<span className={styles.footerText}>
+			Не копируйте пожалуйста
+			<br />
+			{"(⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)"}
+		</span>
 		<a href={"https://github.com/artslde"} target={"_blank"}>
 			<Image
 				source={"/portfolio/github-logo.svg"}
